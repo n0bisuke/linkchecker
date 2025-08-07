e99c7736d7a9f0c7a6dacd702c4b4bc16bdb7fde
n0bisuke <rsksound@gmail.com>
2025-08-08 01:52:25 +0900

    update

--- pre-install.js
+++ pre-install.js
@@ -1,11 +1,16 @@
 const { execSync } = require('child_process');
+const path = require('path');
+
+// アクションのファイルが置かれているディレクトリを取得します
+const actionDir = __dirname;
 
 try {
-  console.log('Running npm install...');
-  execSync('npm install', { stdio: 'inherit' });
+  console.log(`Running npm install in ${actionDir}...`);
+  // `cwd`オプションで、npm installの実行場所をアクションのディレクトリに指定します
+  execSync('npm install', { cwd: actionDir, stdio: 'inherit' });
   console.log('npm install completed successfully.');
 } catch (error) {
-  console.error('Failed to run npm install:');
+  console.error(`Failed to run npm install in ${actionDir}:`);
   console.error(error);
   process.exit(1);
 }
